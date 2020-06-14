308a667b9856a201a4c728f375e658f700477f6a
Oleksii Novikov <aleguna@gmail.com>
2020-06-14 17:28:57 +0100

    dynamic list of selected files in import dialog

--- filmlog.lrdevplugin/import.lua
+++ filmlog.lrdevplugin/import.lua
@@ -1,8 +1,43 @@
+local LrFunctionContext = import 'LrFunctionContext'
+local LrBinding = import 'LrBinding'
 local LrDialogs = import 'LrDialogs'
+local LrView = import 'LrView'
+local LrApplication = import 'LrApplication'
+local LrTasks = import 'LrTasks'
 
-MyHWExportItem = {}
-function MyHWExportItem.showModalDialog()
-    LrDialogs.message( "ExportMenuItem Selected", "Hello World!", "info" )
+function showFilmShotsImportDialog () 
+    LrFunctionContext.postAsyncTaskWithContext ("showFilmShotsImportDialog", function (context)
+        local f = LrView.osFactory()
+
+        local catalog = LrApplication.activeCatalog ()
+        local selection = catalog:getMultipleSelectedOrAllPhotos ()
+
+        local content_array = {
+            spacing = f:dialog_spacing()
+        }
+
+        for i, photo in ipairs (selection) do
+            table.insert (content_array, f:row {
+                fill_horizontal  = 1,
+                f:static_text {
+					alignment = "right",
+					width = LrView.share "label_width",
+					title = "File Name"
+				},
+                f:static_text {
+					title = photo:getFormattedMetadata ('fileName')
+				},
+            })
+        end
+
+        local content = f:column (content_array)
+    
+        LrDialogs.presentModalDialog {
+            title = "Custom Dialog Observer",
+            contents = content
+        }
+
+    end)
 end
 
-MyHWExportItem.showModalDialog()
+showFilmShotsImportDialog ()
