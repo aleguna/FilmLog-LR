9345d49922aa5aa49afef6d997e5e23de5e5cb2d
Oleksii Novikov <aleguna@gmail.com>
2020-06-14 10:34:57 +0100

    All matadata fields

--- filmlog.lrdevplugin/FilmShotsMetadataDefinition.lua
+++ filmlog.lrdevplugin/FilmShotsMetadataDefinition.lua
@@ -1,16 +1,171 @@
 return {
     metadataFieldsForPhotos = {
         {
-            id = 'Locality',
-            title = "Locality",
-            dataType = 'string', -- Specifies the data type for this field.
+            id = 'Roll_UID',
+        },
+
+        {
+            id = "Roll_Mode",
+        },
+
+        {
+            id = "Roll_Status",
+        },
+
+        {
+            id = 'Roll_Name',
+            title = "Roll Name",
+            dataType = 'string', 
             browsable = true,
             searchable = true,
         },
+
+        {
+            id = 'Roll_Comment',
+            title = "Roll Comment",
+            dataType = 'string', 
+            browsable = true,
+            searchable = true,
+        },
+
+        {
+            id = "Roll_Thumbnail"
+        },
+
+        {
+            id = "Roll_CreationTimeUnix"
+        },
+
+        {
+            id = 'Roll_CameraName',
+            title = "Camera Name",
+            dataType = 'string', 
+            browsable = true,
+            searchable = true,
+        },
+
+        {
+            id = 'Roll_FormatName',
+            title = "Format Name",
+            dataType = 'string', 
+            browsable = true,
+            searchable = true,
+        },
+
+        {
+            id = 'Frame_Index',
+            title = "Frame Index",
+            dataType = 'string', 
+            browsable = true,
+            searchable = true,
+        },
+
+        {
+            id = 'Frame_LocalTimeIso8601',
+            title = "Local Time",
+            dataType = 'string',
+            browsable = true,
+            searchable = true,
+        },
+
+        {
+            id = 'Frame_Thumbnail',
+           
+        },
+
+        {
+            id = 'Frame_Latitude',
+            title = "Latitude",
+            dataType = 'string',
+            browsable = true,
+            searchable = true,
+        },
+
+        {
+            id = 'Frame_Longitude',
+            title = "Longitude",
+            dataType = 'string',
+            browsable = true,
+            searchable = true,
+        },
+
+        {
+            id = 'Frame_Locality',
+            title = "Title or Locality",
+            dataType = 'string',
+            browsable = true,
+            searchable = true,
+        },
+
+        {
+            id = 'Frame_Comment',
+            title = "Comment",
+            dataType = 'string',
+            browsable = true,
+            searchable = true,
+        },
+
+        {
+            id = 'Frame_EmulsionName',
+            title = "Emulsion",
+            dataType = 'string', 
+            browsable = true,
+            searchable = true,
+        },
+
+        {
+            id = 'Frame_BoxISO',
+            title = "Box ISO",
+            dataType = 'string', 
+            browsable = true,
+            searchable = true,
+        },
+
+        {
+            id = 'Frame_RatedISO',
+            title = "Rated ISO",
+            dataType = 'string', 
+            browsable = true,
+            searchable = true,
+        },
+
+        {
+            id = 'Frame_LensName',
+            title = "Lens Name",
+            dataType = 'string', 
+            browsable = true,
+            searchable = true,
+        },
+
+        {
+            id = 'Frame_FocalLength',
+            title = "Focal Length",
+            dataType = 'string', 
+            browsable = true,
+            searchable = true,
+        },
+
+        {
+            id = 'Frame_FStop',
+            title = "F-Stop",
+            dataType = 'string', 
+            browsable = true,
+            searchable = true,
+        },
+
+        {
+            id = 'Frame_Shutter',
+            title = "Shutter Speed",
+            dataType = 'string', 
+            browsable = true,
+            searchable = true,
+        },
+
+      
     },
 
     schemaVersion = 1,
 	
-    updateFromEarlierSchemaVersion = function( catalog, previousSchemaVersion )
+    updateFromEarlierSchemaVersion = function (catalog, previousSchemaVersion)
     end,
 }
