713215b81d7643dec9edf7641a9ad6a6019fe4e2
Oleksii Novikov <aleguna@gmail.com>
2021-03-02 02:12:00 +0000

    logger table refs

--- filmlog.lrdevplugin/Logger.lua
+++ filmlog.lrdevplugin/Logger.lua
@@ -1,111 +1,125 @@
-
 STRING_LIMIT = 2048
 
 local loggers = {
     entry = nil,
-    map = {
-    }
+    map = {}
 }
 
-local function serializeKeyval (file, key, value, indent, serialize)
-    file:write(string.format(indent.."[%q] = ", key))
-    serialize(file, value, indent)   
+local serialize, serializeKeyval, serializeTable
+
+local function serializeKeyval(file, key, value, indent, tables)
+    file:write(string.format(indent .. "[%q] = ", key))
+    serialize(file, value, indent, tables)
 end
 
-local function serialize (file, o, indent)
+local function serializeTable (file, o, indent, tables)
+    file:write("{\n")
+    local newIndent = indent .. " "
+    for k, v in pairs(o) do
+        serializeKeyval(file, k, v, newIndent, tables)
+        file:write("\n")
+    end
+    file:write(indent .. "}")
+end
+
+serialize = function (file, o, indent, tables)
     if o then
-        local t = type (o)
-        if t == 'number' then
+        local t = type(o)
+        if t == "number" then
             file:write(o)
-        elseif t == 'string' then
-            if o:len () > STRING_LIMIT then
-                file:write (o:sub (1, STRING_LIMIT), "<...>")
+        elseif t == "string" then
+            if o:len() > STRING_LIMIT then
+                file:write(o:sub(1, STRING_LIMIT), "<...>")
             else
                 file:write(o)
             end
-        elseif t == 'table' then
-            file:write('{\n')
-            local newIndent = indent..'\t'
-            for k,v in pairs(o) do
-                serializeKeyval (file, k, v, newIndent, serialize)
-                file:write ('\n')
+        elseif t == "table" then
+            local tableAddress = tostring(o)
+            if tables[tableAddress] then
+                file:write(indent, tableAddress)
+            else
+                tables[tableAddress] = true
+                serializeTable (file, o, indent, tables)
             end
-            file:write(indent..'}')
-        elseif t == 'boolean' then
-            file:write (o and 'true' or 'false')            
+        elseif t == "boolean" then
+            file:write(o and "true" or "false")
         else
-            file:write ('<<', t, '>>')
+            file:write("<<", t, ">>")
         end
     else
-        file:write ('nil')
+        file:write("nil")
     end
 end
 
-local function getLogSuffix ()
-    local pu = import 'LrPathUtils'
-    local configPath = pu.child (_PLUGIN.path, 'Config.txt')
+local function getLogSuffix()
+    local pu = import "LrPathUtils"
+    local configPath = pu.child(_PLUGIN.path, "Config.txt")
 
     local config = {}
-    local configChunk = loadfile (configPath)
+    local configChunk = loadfile(configPath)
     if configChunk then
-        config = configChunk ()
+        config = configChunk()
     end
     return config.LogFile
 end
 
-local function writeLog (logPath, prefix, ...)    
+local function writeLog(logPath, prefix, ...)
     local args = {...}
     if #args > 0 and logPath then
-        local f = io.open (logPath, 'a')
+        local f = io.open(logPath, "a")
         if f then
-            f:write ('<', prefix, '>: ')
-            for _, arg in ipairs (args) do
-                serialize (f, arg, '')
-                f:write (' ')
+            f:write("<", prefix, ">: ")
+            for _, arg in ipairs(args) do
+                serialize(f, arg, "", {})
+                f:write(" ")
             end
-            f:write ('\n')
-            f:close ()            
-        end        
+            f:write("\n")
+            f:close()
+        end
     end
 end
 
-local function getLogger (prefix)
+local function getLogger(prefix)
     if not prefix then
-        return function (...) end
+        return function(...)
+        end
     end
 
-    local logger = loggers [prefix]
+    local logger = loggers[prefix]
     local isEntry = false
 
-    if not logger then 
-        logger = function (...) end
+    if not logger then
+        logger = function(...)
+        end
         if loggers.entry == nil then
             loggers.entry = prefix
             isEntry = true
         end
 
         if _G.print and not _PLUGIN then
-            if os and os.getenv and os.getenv ('ENABLE_LOGGING') then        
-                logger = function (...) 
-                    print ('<', prefix, '>: ', ...)
+            if os and os.getenv and os.getenv("ENABLE_LOGGING") then
+                os.execute ("mkdir -p test/log")
+                local logPath = "test/log/" .. prefix .. ".log" 
+                logger = function(...)
+                    writeLog(logPath, prefix, ...)
                 end
-            end            
+            end
         elseif _PLUGIN and _PLUGIN.path then
-            local suffix = getLogSuffix ()
+            local suffix = getLogSuffix()
             if suffix then
-                local pu = import 'LrPathUtils'
-                local fu = import 'LrFileUtils'        
+                local pu = import "LrPathUtils"
+                local fu = import "LrFileUtils"
 
-                local logDir = pu.child (_PLUGIN.path, "log")
-                if not fu.exists (logDir) then
-                    fu.createDirectory (logDir)
+                local logDir = pu.child(_PLUGIN.path, "log")
+                if not fu.exists(logDir) then
+                    fu.createDirectory(logDir)
                 end
 
-                local logPath = pu.addExtension (pu.child (logDir, loggers.entry .. "_" .. suffix), "txt")
-                logger = function (...) 
-                    writeLog (logPath, prefix, ...)
+                local logPath = pu.addExtension(pu.child(logDir, loggers.entry .. "_" .. suffix), "txt")
+                logger = function(...)
+                    writeLog(logPath, prefix, ...)
                 end
-            end        
+            end
         end
 
         loggers[prefix] = logger
@@ -114,10 +128,10 @@ local function getLogger (prefix)
     return logger, isEntry
 end
 
-local function startLog (prefix)
-    local log, entry = getLogger (prefix)
+local function startLog(prefix)
+    local log, entry = getLogger(prefix)
     if entry then
-        log ("================== <BEGIN> ==================")
+        log("================== <BEGIN> ==================")
     end
     return log
 end
